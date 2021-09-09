3e411eda6071a7932f72f91530d6022f747c8caa
Thomas Schuetz <38893055+thschue@users.noreply.github.com>
2021-09-09 13:12:30 +0200

    Update version.go

--- ci-connect-cli/cmd/version.go
+++ ci-connect-cli/cmd/version.go
@@ -6,12 +6,12 @@ import (
 	"github.com/spf13/cobra"
 )
 
-const version = "0.0.7"
+const version = "1.0.0"
 
 var versionCmd = &cobra.Command{
 	Use:   "version",
-	Short: "Print the version number of the KIA CLI",
-	Long:  `All software has versions. This is KIA CLI's'`,
+	Short: "Print the version number of the CI Connect CLI",
+	Long:  `All software has versions. This is CI Connect CLI's'`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println(version)
 	},
